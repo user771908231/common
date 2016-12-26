3f14b46510cce9794068c79eb4d09935044df167
james wang <yiming.520@live.com>
2016-12-26 12:36:47 +0800

    新增商城商品列表Proto协议

--- common/consts/tableName/mongoTableName.go
+++ common/consts/tableName/mongoTableName.go
@@ -5,5 +5,5 @@ const (
 	DBT_T_USER_DIAMOND_DETAILS string = "t_user_diamond_details"
 	DBT_T_TH_NOTICE = "t_th_notice"
 	DBT_T_RECHARGE_DETAILS string = "t_recharge_details"
-
+	DBT_T_USER_TASK = "t_user_task"
 )
