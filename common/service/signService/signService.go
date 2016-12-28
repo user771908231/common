d5bc5b67d9cc2cea1c9b8edded19bc09c970325d
沈厚桦 <495430312@qq.com>
2016-12-28 16:53:20 +0800

    timeUtils 比较是否是同一天的方法 新增请求转盘签到奖励物品信息的pid

--- common/service/signService/signService.go
+++ common/service/signService/signService.go
@@ -9,6 +9,7 @@ import (
 	"github.com/golang/protobuf/proto"
 	"casino_common/common/userService"
 	"github.com/name5566/leaf/gate"
+	"casino_common/utils/timeUtils"
 )
 
 
@@ -29,7 +30,7 @@ func DoSign(user *ddproto.User) error {
 
 		// 每天只能签一次
 		//上次签到的日期(天)等于当前日期(天)
-		if IsSameDate(lastSignTime, timeNow) {
+		if timeUtils.EqualDate(lastSignTime, timeNow) {
 			log.T("上次签到日期[%v]等于当前日期[%v]", user.GetLastSignTime(), timeNow.Format("2006-01-02 15:04:05"))
 			return Error.NewError(-1, "上次签到日期等于当前日期, 签到失败")
 		}
@@ -90,15 +91,5 @@ func deliveryUserSignLottery(user *ddproto.User) error {
 func IsUserSignedToday(u *ddproto.User) bool {
 	timeNow := time.Now()
 	lastSignTime, _ := time.Parse(u.GetLastSignTime(), "2006-01-02 15:04:05")
-	return IsSameDate(lastSignTime, timeNow)
-}
-
-//判断两个日期是否是同一天
-func IsSameDate(time1 time.Time, time2 time.Time) bool {
-	if time1.Year() == time2.Year() &&
-		time1.Month() == time2.Month() &&
-		time1.Day() == time2.Day() {
-		return true
-	}
-	return false
+	return timeUtils.EqualDate(lastSignTime, timeNow)
 }
