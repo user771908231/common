a40dea1e494aa90b6d1a8ea2de8aa2c6d8e7e268
saplmm <1046439010@qq.com>
2016-12-28 20:34:24 +0800

    修改 proto 增加assets optional bool isCode = 7;    //源码在根目录;非源码文件在res/raw-assets/resources目录 }

--- common/log/loginit.go
+++ common/log/loginit.go
@@ -5,7 +5,7 @@ import "os"
 func InitLogger(logPath, logName string) {
 	//配置log 文件.这里可以通过conf.json 来进行配置
 	SetMaxFileCount(int32(20))
-	SetMaxFileSize(int64(50), MB)
+	SetMaxFileSize(int64(5), MB)
 	SetLevel(ALL)
 	SetConsole(true)
 	SetDebug(true)
