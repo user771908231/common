7fc99a5c2126267d7894d0116e280467e06b8ff7
saplmm <1046439010@qq.com>
2017-01-06 15:05:57 +0800

    no message

--- common/sessionService/session.go
+++ common/sessionService/session.go
@@ -11,14 +11,18 @@ import (
 )
 
 // session相关的...
+/**
+	目前的session每个人将会保存两个：朋友桌和金币场
+
+ */
 const (
 	GAMESESSION_STATUS_NOGAME int32 = 1; //没有在游戏中
 	GAMESESSION_STATUS_GAMING int32 = 2; //没有在游戏中
 
-	GAMEID_DEZHOU    int32 = 1; //德州
-	GAMEID_MAJAING   int32 = 2; //麻将
-	GAMEID_DOUZIZHU  int32 = 3; //斗地主
-	GAMEID_ZHAJINHUA int32 = 4; //这金花
+	GAMEID_DEZHOU    int32 = 1                                         //德州
+	GAMEID_MAJAING   int32 = int32(ddproto.CommonEnumGame_GID_MAHJONG) //麻将
+	GAMEID_DOUZIZHU  int32 = int32(ddproto.CommonEnumGame_GID_DDZ)     //斗地主
+	GAMEID_ZHAJINHUA int32 = int32(ddproto.CommonEnumGame_GID_ZJH)     //扎金花
 )
 
 var MJSESSION_KEY_PRE = "redis_game_session"
@@ -30,8 +34,9 @@ func getSessionKey(userId uint32, gameId int32) string {
 	return ret
 }
 
-func GetSession(userId uint32, gameId int32) *ddproto.GameSession {
-	s := redisUtils.GetObj(getSessionKey(userId, gameId), &ddproto.GameSession{})
+//
+func GetSession(userId uint32, roomType int32) *ddproto.GameSession {
+	s := redisUtils.GetObj(getSessionKey(userId, roomType), &ddproto.GameSession{})
 	if s != nil {
 		return s.(*ddproto.GameSession)
 	} else {
@@ -41,7 +46,7 @@ func GetSession(userId uint32, gameId int32) *ddproto.GameSession {
 
 //更新用户的session信息，具体更新什么信息待定
 func UpdateSession(userId uint32, gameStatus int32, gameId int32, gameNumber int32, roomId int32, deskId int32, gameCustomStatus int32, isBreak bool, isLeave bool, roomType int32, roomPass string) (*ddproto.GameSession, error) {
-	session := GetSession(userId, gameId)
+	session := GetSession(userId, roomType)
 	if session == nil {
 		log.E("没有找到user[%v]的session,需要重新申请一个并保存...", userId)
 		session = commonNewPorot.NewGameSession()
