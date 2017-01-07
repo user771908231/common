9ab8ca12a38010d6017225c9a8a7aced78a48606
saplmm <1046439010@qq.com>
2017-01-07 15:16:19 +0800

    删除 限定苹果支付相关的代码，由前端来判断....

--- common/handlers/wxPayHandlers.go
+++ common/handlers/wxPayHandlers.go
@@ -6,8 +6,6 @@ import (
 	"casino_common/proto/ddproto"
 	"casino_common/utils/agentUtils"
 	"casino_hall/wxpay"
-	"casino_common/common/sys"
-	"github.com/golang/protobuf/proto"
 )
 
 //处理统一下单
@@ -16,16 +14,7 @@ func HandlerWxpayUnifiedOrder(args []interface{}) {
 	a := args[1].(gate.Agent)
 	//通过meal知道 充值的数目等
 	ack, _ := service.GetAppWxpayReqParams(m.GetPayModelId(), m.GetProductId(), m.GetHeader().GetUserId(), agentUtils.GetIP(a), wxpay.DEFAULT_DEVICEINFO)
-	if sys.GAMEENV.APPLEPAY_ONLY {
-		//方便上架..设置使用苹果支付
-		ack = new(ddproto.WxpayAckUnifiedorder)
-		ack.Header = new(ddproto.ProtoHeader)
-		ack.Header.Code = proto.Int32(int32(ddproto.PayReturn_ERR_ONLY_APPLE))
-		a.WriteMsg(ack)
-	} else {
-		//生产环境使用微信支付...
-		a.WriteMsg(ack)
-	}
+	a.WriteMsg(ack)
 }
 
 //app 同步返回之后请求轮询
