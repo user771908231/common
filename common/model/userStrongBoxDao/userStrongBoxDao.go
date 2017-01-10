1352bc5a5a0148e13f8b868cb061f6e3bd5a4606
saplmm <1046439010@qq.com>
2017-01-10 15:46:00 +0800

    保险箱处理已经完成

--- common/model/userStrongBoxDao/userStrongBoxDao.go
+++ common/model/userStrongBoxDao/userStrongBoxDao.go
@@ -20,13 +20,18 @@ func FindUserStrongBoxByKV(key string, v interface{}) *model.T_user_strongBox {
 }
 
 func FindUserStrongBoxByUserId(userId uint32) *model.T_user_strongBox {
-	return FindUserStrongBoxByKV("UserId", userId)
+	return FindUserStrongBoxByKV("userid", userId)
+}
+
+//插入一条数据
+func InsertUserStrongBox(t *model.T_user_strongBox) {
+	db.InsertMgoData(tableName.DBT_T_USER_STRONGBOX, t)
 }
 
 func UpdateUserStrongBoxByModel(t *model.T_user_strongBox) {
-	db.Query(func(d *mgo.Database){
+	db.Query(func(d *mgo.Database) {
 		d.C(tableName.DBT_T_USER_STRONGBOX).Update(bson.M{
-			"userId": t.UserId,
+			"userid": t.UserId,
 		}, t)
 	})
 }
