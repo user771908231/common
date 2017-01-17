f99ab1190b1ab02ebc1c019fd2ac6cbb72b36f41
saplmm <1046439010@qq.com>
2017-01-17 19:26:43 +0800

    增加redis_utils 的测试代码

--- utils/redisUtils/reids_int_test.go
+++ utils/redisUtils/reids_int_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"casino_common/utils/redis"
 	"casino_common/common/log"
+	"sync/atomic"
 )
 
 func init() {
@@ -12,13 +13,26 @@ func init() {
 	log.InitLogger("", "") //初始化日志处理
 }
 
+var count int32 = 0
+
 func TestData_Open(t *testing.T) {
-	for i := 1; i < 100000; i++ {
+
+	done := make(chan bool)
+	max := int32(100000)
+	for i := 1; i < int(max); i++ {
 		fmt.Printf("%v ...........\n", i)
 		go func() {
 			r := GetConn()
-			fmt.Println("处理...")
+			atomic.AddInt32(&count, 1)
+			fmt.Printf("处理...%v \n", count)
 			r.Close()
+			if count > (max - 2 ) {
+				done <- true
+			}
 		}()
 	}
+
+	<-done
+	fmt.Println("最后的结果...", count)
+
 }
