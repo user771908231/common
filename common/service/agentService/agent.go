dbff58321ee109454bdb3e3f71c47cdbe970b0b1
james wang <yiming.520@live.com>
2017-03-04 18:03:50 +0800

    新增push协议及pid，AgentMap相关Get()Set()方法

---
+++ common/service/agentService/agent.go
@@ -0,0 +1,34 @@
+package agentService
+
+import (
+	"github.com/name5566/leaf/gate"
+	"errors"
+	"casino_common/common/log"
+)
+
+func init() {
+	AgentMap = map[uint32]gate.Agent{}
+}
+
+var AgentMap map[uint32]gate.Agent
+
+func GetAgent(userId uint32) (gate.Agent, error) {
+	log.T("get %v", AgentMap)
+	a,ok := AgentMap[userId]
+	if ok {
+		return a, nil
+	}
+	return nil, errors.New("not found agent.")
+}
+
+func SetAgent(userId uint32, a gate.Agent) {
+	AgentMap[userId] = a
+	log.T("set %v", AgentMap)
+}
+
+func DelAgent(userId uint32) {
+	_,ok := AgentMap[userId]
+	if ok {
+		delete(AgentMap, userId)
+	}
+}
