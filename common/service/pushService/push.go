dd5a97b40e4d4d8d81d07d2ca9bd4444a19af3d1
james wang <yiming.520@live.com>
2017-03-15 15:09:00 +0800

    向大厅推送消息，使断线后第一时间重新建立连接。

--- common/service/pushService/push.go
+++ common/service/pushService/push.go
@@ -5,9 +5,16 @@ import (
 	"casino_common/proto/ddproto"
 	"gopkg.in/fatih/pool.v2"
 	"casino_super/conf"
+	"errors"
 )
 
 func init() {
+	//初始化连接池
+	PoolInit()
+}
+
+//初始化连接池
+func PoolInit() error {
 	//此处注释以便单机运行
 	factory := func() (net.Conn, error) {
 		return net.Dial("tcp", conf.Server.HallTcpAddr)
@@ -15,22 +22,51 @@ func init() {
 	var err error = nil
 	PoolStack, err = pool.NewChannelPool(5, 30, factory)
 	if err != nil {
-		panic("pool init fail.")
+		return errors.New("pool init err.")
 	}
+	return nil
 }
 
 var PoolStack pool.Pool
 
+//向大厅推送消息
+func Push(data []byte) error {
+	//如果遇到错误，则尝试重新初始化连接池，保证与大厅断线后可以第一时间重连。
+	if PoolStack == nil {
+		if PoolInit() == nil {
+			go Push(data)
+			return nil
+		}
+		return errors.New("push err: PoolStack nil.")
+	}
+
+	conn,err := PoolStack.Get()
+	defer conn.Close()
+
+	if err != nil {
+		if PoolInit() == nil {
+			go Push(data)
+			return nil
+		}
+		return errors.New("push err: Get err")
+	}
+
+	_,err = conn.Write(data)
+	if err != nil {
+		if PoolInit() == nil {
+			go Push(data)
+			return nil
+		}
+		return errors.New("push err: Write err.")
+	}
+
+	return nil
+}
+
 //向大厅服务器推送消息
 func PushUserData(userId uint32) error {
-	conn,err := PoolStack.Get()
-	if err != nil {
-		return err
-	}
 	data := AssembleData(ddproto.HallEnumProtoId_HALL_PID_PUSH_REQ, &ddproto.Push{
 		Id:&userId,
 	})
-	_,err = conn.Write(data)
-	defer conn.Close()
-	return err
+	return Push(data)
 }
