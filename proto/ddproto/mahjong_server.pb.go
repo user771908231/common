9cf48c7ab0af2eb5fb2efc8a13e9cb5401d16af6
saplmm <1046439010@qq.com>
2017-03-23 17:18:30 +0800

    proto重新编译...

--- proto/ddproto/mahjong_server.pb.go
+++ proto/ddproto/mahjong_server.pb.go
@@ -13,8 +13,6 @@ var _ = proto.Marshal
 var _ = fmt.Errorf
 var _ = math.Inf
 
-func init() { proto.RegisterFile("mahjong_server.proto", fileDescriptor20) }
-
 var fileDescriptor20 = []byte{
 	// 52 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x09, 0x6e, 0x88, 0x02, 0xff, 0xe2, 0x12, 0xc9, 0x4d, 0xcc, 0xc8,
