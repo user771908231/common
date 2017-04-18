97065b691f37ed6b2e8d931bf7f6f3a8cd2418ed
saplmm <1046439010@qq.com>
2017-04-18 17:17:52 +0800

    修改测试代码

--- utils/dingding/dingding_test.go
+++ utils/dingding/dingding_test.go
@@ -6,13 +6,6 @@ import (
 )
 
 func TestDingMessage_Send(t *testing.T) {
-	for i := 0; i < 100; i++ {
-		DingMsger.Send("测试1、\"")
-		DingMsger.Send("测试2'''sfsfwe")
-		DingMsger.Send("测试3[desk-哈哈---]sfasfafa''' sfsaf   ")
-		DingMsger.Send("测试4\tfsahflalaf")
-		DingMsger.Send("测试5")
-		DingMsger.Send("测试6")
-	}
+	DingMsger.Send("测试1、\"")
 	time.Sleep(time.Second * 10)
 }
