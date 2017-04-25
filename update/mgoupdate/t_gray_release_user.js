742a62c63dc7efe1fc853e2fed396e64ccd26e05
saplmm <1046439010@qq.com>
2017-04-25 13:53:19 +0800

    更新灰度发布的白名单...

--- update/mgoupdate/t_gray_release_user.js
+++ update/mgoupdate/t_gray_release_user.js
@@ -13,3 +13,32 @@ db.t_gray_release_user.insert({
     "userid": 11205,
     "status": 1,
 });
+
+db.t_gray_release_user.insert({
+    "id": 2,
+    "userid": 11977,
+    "status": 1,
+});
+
+db.t_gray_release_user.insert({
+    "id": 3,
+    "userid": 11564,
+    "status": 1,
+});
+
+db.t_gray_release_user.insert({
+    "id": 4,
+    "userid": 10356,
+    "status": 1,
+});
+
+db.t_gray_release_user.insert({
+    "id": 5,
+    "userid": 11533,
+    "status": 1,
+});
+
+//  11977 QWERTY 邓艳梅
+//  11564 小药丸  钟春燕
+//  10356 kory  杨荣华
+//  11533 一根藤上七个娃娃 iPhone6
