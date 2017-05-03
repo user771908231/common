681dfde447573810ed7ccacd09903ce743a27b1d
沈厚桦 <495430312@qq.com>
2017-05-03 16:31:53 +0800

    编译错误修复

--- common/model/PDKDeskRoundDao/PdkDeskRoundDao.go
+++ common/model/PDKDeskRoundDao/PdkDeskRoundDao.go
@@ -11,7 +11,7 @@ import (
 )
 
 func GetPdkDeskRoundByUserId(userId uint32) []model.T_pdk_desk_round {
-	var deskRecords []model.T_ddz_desk_round
+	var deskRecords []model.T_pdk_desk_round
 	querKey, _ := numUtils.Uint2String(userId)
 	db.Query(func(d *mgo.Database) {
 		d.C(tableName.DBT_PDK_DESK_ROUND).Find(bson.M{"userids": bson.RegEx{querKey, "."}}).Sort("-deskid").Limit(20).All(&deskRecords)
