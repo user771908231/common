f0fe2530842c234cfe609140f677e20a921c5d49
沈厚桦 <495430312@qq.com>
2017-05-04 15:27:10 +0800

    跑得快回放战绩的更新 编译proto

---
+++ common/model/paodekuai/t_pdk_desk_round_playback.go
@@ -0,0 +1,32 @@
+package paodekuai
+
+import (
+	"casino_common/proto/ddproto"
+	"github.com/golang/protobuf/proto"
+	"time"
+	"casino_common/utils/timeUtils"
+	"gopkg.in/mgo.v2"
+	"casino_common/common/consts/tableName"
+	"gopkg.in/mgo.v2/bson"
+	"casino_common/utils/db"
+)
+
+//回放可以通过这个表来查询
+type T_pdk_desk_round_playback struct {
+	DeskId     int32
+	GameNumber int32
+	PlayBackData []*ddproto.PdkPlaybackSnapshot
+}
+
+func GetPdkPlayBack(gamenumber int32) []*ddproto.PdkPlaybackSnapshot {
+	ret := &T_pdk_desk_round_playback{}
+	db.Query(func(d *mgo.Database) {
+		d.C(tableName.DBT_PDK_DESK_ROUND_PLAYBACK).Find(bson.M{"gamenumber": gamenumber}).One(ret)
+	})
+	if ret.DeskId > 0 {
+		return ret.PlayBackData
+	} else {
+		return nil
+	}
+}
+
