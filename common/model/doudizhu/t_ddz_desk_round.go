32d770ecf7501a726be91cb99b819d48d44b04e9
沈厚桦 <495430312@qq.com>
2017-05-10 17:35:25 +0800

    跑得快 斗地主 四川长沙麻将 新增汇总战绩

--- common/model/doudizhu/t_ddz_desk_round.go
+++ common/model/doudizhu/t_ddz_desk_round.go
@@ -1,16 +1,16 @@
 package doudizhu
 
 import (
-	"casino_common/proto/ddproto"
-	"github.com/golang/protobuf/proto"
-	"time"
-	"casino_common/utils/timeUtils"
-	"casino_common/utils/numUtils"
-	"gopkg.in/mgo.v2"
 	"casino_common/common/consts/tableName"
-	"gopkg.in/mgo.v2/bson"
-	"casino_common/utils/db"
 	"casino_common/common/log"
+	"casino_common/proto/ddproto"
+	"casino_common/utils/db"
+	"casino_common/utils/numUtils"
+	"casino_common/utils/timeUtils"
+	"github.com/golang/protobuf/proto"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"time"
 )
 
 //一把结束,战绩可以通过这个表来查询
@@ -21,6 +21,7 @@ type T_ddz_desk_round struct {
 	BeginTime  time.Time
 	EndTime    time.Time
 	Records    []DdzRecordBean
+	RoundStr   string //局数信息
 }
 
 func (t T_ddz_desk_round) TransRecord() *ddproto.BeanGameRecord {
@@ -28,6 +29,7 @@ func (t T_ddz_desk_round) TransRecord() *ddproto.BeanGameRecord {
 		BeginTime: proto.String(timeUtils.Format(t.BeginTime)),
 		DeskId:    proto.Int32(t.DeskId),
 		Id:        proto.Int32(t.GameNumber),
+		RoundStr:  proto.String(t.RoundStr), //局数信息
 	}
 	for _, bean := range t.Records {
 		b := bean.TransBeanUserRecord()
@@ -51,7 +53,6 @@ func (b DdzRecordBean) TransBeanUserRecord() *ddproto.BeanUserRecord {
 	return result
 }
 
-
 //DAO
 func GetDdzDeskRoundByUserId(userId uint32) []T_ddz_desk_round {
 	var deskRecords []T_ddz_desk_round
@@ -62,6 +63,25 @@ func GetDdzDeskRoundByUserId(userId uint32) []T_ddz_desk_round {
 
 	if deskRecords == nil || len(deskRecords) <= 0 {
 		log.T("没有找到玩家[%v]斗地主相关的战绩...", userId)
+		return nil
+	} else {
+		return deskRecords
+	}
+}
+
+//查询牌桌内战绩
+func GetDdzDeskRoundByDeskId(userId uint32, deskId int32) []T_ddz_desk_round {
+	var deskRecords []T_ddz_desk_round
+	querKey, _ := numUtils.Uint2String(userId)
+	db.Query(func(d *mgo.Database) {
+		d.C(tableName.DBT_DDZ_DESK_ROUND_ALL).Find(bson.M{
+			"userids": bson.RegEx{querKey, "."},
+			"deskid": deskId,
+		}).Sort("-deskid").Limit(20).All(&deskRecords)
+	})
+
+	if deskRecords == nil || len(deskRecords) <= 0 {
+		log.T("没有找到玩家[%v]斗地主相关的牌桌内战绩...", userId)
 		return nil
 	} else {
 		return deskRecords
