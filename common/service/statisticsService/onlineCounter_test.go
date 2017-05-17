047ccfaf56c15815917020e03063d01b95002e9d
saplmm <1046439010@qq.com>
2017-05-17 21:09:55 +0800

    增加单元测试..

---
+++ common/service/statisticsService/onlineCounter_test.go
@@ -0,0 +1,17 @@
+package statisticsService
+
+import (
+	"casino_common/common/log"
+	"casino_common/utils/redis"
+	"testing"
+)
+
+func init() {
+	data.InitRedis("192.168.2.145:6379", "test")
+	log.InitLogger("", "") //初始化日志处理
+}
+
+func TestOnlineCountAll(t *testing.T) {
+	count := OnlineCountAll()
+	t.Logf("总共的数量:%v", count)
+}
