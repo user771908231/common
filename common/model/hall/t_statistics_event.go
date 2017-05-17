f058ff6932d16f27159e121403828d8114a6ba91
saplmm <1046439010@qq.com>
2017-05-17 15:27:26 +0800

    增加事件的数据结构

--- common/model/hall/t_statistics_event.go
+++ common/model/hall/t_statistics_event.go
@@ -1,10 +1,16 @@
 package hall
 
+import "time"
+
 //时间记录表
 
 const (
-	EVENT_DISSOLVE = 1001 //解散房间
+	EVENT_DISSOLVE int32 = 1001 //解散房间
 )
 
 type T_statistics_event struct {
+	EventId   int32     //事件id
+	EventDes  string    //事件描述
+	EventUser uint32    //操作时间的玩家
+	Time      time.Time //操作事件的时间
 }
