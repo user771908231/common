902d21987bab706720fe73f93aecf20ca1391a28
james wang <yiming.520@live.com>
2017-06-09 16:25:21 +0000

    牛牛战绩查询改为从日志服务器查。

--- common/model/NiuDeskRoundDao/NiuDeskRoundDao.go
+++ common/model/NiuDeskRoundDao/NiuDeskRoundDao.go
@@ -14,9 +14,8 @@ import (
 func GetNiuDeskRoundByUserId(userId uint32) []model.T_niuniu_desk_round {
 	var deskRecords []model.T_niuniu_desk_round
 	querKey, _ := numUtils.Uint2String(userId)
-	db.Query(func(d *mgo.Database) {
-		d.C(tableName.DBT_NIU_DESK_ROUND_ALL).Find(bson.M{"userids": bson.RegEx{querKey, "."}}).Sort("-endtime").Limit(20).All(&deskRecords)
-	})
+
+	db.Log(tableName.DBT_NIU_DESK_ROUND_ALL).Page(bson.M{"userids": bson.RegEx{querKey, "."}}, &deskRecords, "-endtime", 1, 20)
 
 	if deskRecords == nil || len(deskRecords) <= 0 {
 		log.T("没有找到玩家[%v]牛牛相关的战绩...", userId)
@@ -30,12 +29,11 @@ func GetNiuDeskRoundByUserId(userId uint32) []model.T_niuniu_desk_round {
 func GetNiuDeskRoundByDeskId(userId uint32, deskId int32) []model.T_niuniu_desk_round {
 	var deskRecords []model.T_niuniu_desk_round
 	querKey, _ := numUtils.Uint2String(userId)
-	db.Query(func(d *mgo.Database) {
-		d.C(tableName.DBT_NIU_DESK_ROUND_ONE).Find(bson.M{
-			"userids": bson.RegEx{querKey, "."},
-			"deskid":  deskId,
-		}).Sort("-gamenumber").Limit(20).All(&deskRecords)
-	})
+
+	db.Log(tableName.DBT_NIU_DESK_ROUND_ONE).Page(bson.M{
+		"userids": bson.RegEx{querKey, "."},
+		"deskid":  deskId,
+	}, &deskRecords, "-gamenumber", 1, 20)
 
 	if deskRecords == nil || len(deskRecords) <= 0 {
 		log.T("没有找到玩家[%v]牛牛相关的牌桌内战绩...", userId)
