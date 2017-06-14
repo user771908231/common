41a11b39b50a7196f5c6d8ccad21693497949169
yons <1095164848@qq.com>
2017-06-14 17:42:10 +0000

    mongo 添加  福袋  表

--- test/test.go
+++ test/test.go
@@ -1,13 +1,13 @@
 package test
 
 import (
-	"casino_common/common/sys"
-	"casino_common/utils/db"
-	"casino_common/common/log"
+	//"casino_common/common/sys"
+	//"casino_common/utils/db"
+	//"casino_common/common/log"
 )
 
 func TestInit() {
-	sys.InitRedis("192.168.199.200:6379", "test")
-	db.Oninit("192.168.199.200", 27017, "test", "id")
-	log.InitLogger("", "") //初始化日志处理
+	//sys.InitRedis("192.168.199.200:6379", "test")
+	//db.Oninit("192.168.199.200", 27017, "test", "id")
+	//log.InitLogger("", "") //初始化日志处理
 }
