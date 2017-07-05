acb8304cd3456f2a585aedb4bc3058b096dc8448
james wang <yiming.520@live.com>
2017-07-05 16:22:56 +0000

    sys.Init()新增日志文件大小配置参数

--- common/log/loginit.go
+++ common/log/loginit.go
@@ -2,10 +2,14 @@ package log
 
 import "os"
 
-func InitLogger(logPath, logName string) {
+func InitLogger(logPath, logName string, logFileSize int32) {
 	//配置log 文件.这里可以通过conf.json 来进行配置
 	SetMaxFileCount(int32(100))
-	SetMaxFileSize(int64(100), MB)
+	if logFileSize <= 0 {
+		SetMaxFileSize(int64(100), MB)
+	}else {
+		SetMaxFileSize(int64(logFileSize), MB)
+	}
 	SetLevel(ALL)
 	SetConsole(true)
 	SetDebug(true)
