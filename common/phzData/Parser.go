3a17440b35ed9932efe2cfcbb46fc491db687253
hong <785131182@qq.com>
2017-10-12 20:29:00 +0800

    修复下标越界

--- common/phzData/Parser.go
+++ common/phzData/Parser.go
@@ -829,7 +829,7 @@ func CanHu(huxi int32, count []int, len int, zimoPaiValue int32) (info CanHuInfo
 			}
 		}
 		//大字 一句话
-		for i := PAIVALUE_SMALL + 1; i < TOTALPAIVALUE_COUNT+1; i++ {
+		for i := PAIVALUE_SMALL + 1; i < TOTALPAIVALUE_COUNT-1; i++ {
 			if count[i] > 0 && count[i+1] > 0 && count[i+2] > 0 {
 				count[i] -= 1
 				count[i+1] -= 1
