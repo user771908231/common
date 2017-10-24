da49c505cbf7c40f22cd8eaf81b5269b25349990
hong <785131182@qq.com>
2017-10-24 15:48:34 +0800

    跑胡子增加战绩、回放、游戏开始结束时间

---
+++ common/model/paohuzi/t_paohuzi_desk_round.go
@@ -0,0 +1,88 @@
+package paodekuai
+
+import (
+	"casino_common/common/consts/tableName"
+	"casino_common/common/log"
+	"casino_common/proto/ddproto"
+	"casino_common/utils/db"
+	"casino_common/utils/numUtils"
+	"casino_common/utils/timeUtils"
+	"github.com/golang/protobuf/proto"
+	"gopkg.in/mgo.v2/bson"
+	"time"
+)
+
+//一把结束,战绩可以通过这个表来查询
+type T_phz_desk_round struct {
+	DeskId     int32
+	Password   string //房间号
+	GameNumber int32
+	UserIds    string
+	BeginTime  time.Time
+	EndTime    time.Time
+	RoundStr   string //局数信息
+	Records    []PhzRecordBean
+}
+
+func (t T_phz_desk_round) TransRecord() *ddproto.BeanGameRecord {
+	result := &ddproto.BeanGameRecord{
+		BeginTime: proto.String(timeUtils.Format(t.BeginTime)),
+		DeskId:    proto.Int32(t.DeskId), //这里实际返回的是房间号
+		Password:  proto.String(t.Password),
+		Id:        proto.Int32(t.GameNumber),
+		RoundStr:  proto.String(t.RoundStr), //局数信息
+	}
+	for _, bean := range t.Records {
+		b := bean.TransBeanUserRecord()
+		result.Users = append(result.Users, b)
+	}
+	return result
+}
+
+type PhzRecordBean struct {
+	UserId    uint32
+	NickName  string
+	WinAmount int64
+}
+
+func (b PhzRecordBean) TransBeanUserRecord() *ddproto.BeanUserRecord {
+	result := &ddproto.BeanUserRecord{
+		NickName:  proto.String(b.NickName),
+		UserId:    proto.Uint32(b.UserId),
+		WinAmount: proto.Int64(b.WinAmount),
+	}
+	return result
+}
+
+//DAO
+func GetPhzDeskRoundByUserId(userId uint32) []T_phz_desk_round {
+	var deskRecords []T_phz_desk_round
+	querKey, _ := numUtils.Uint2String(userId)
+	db.Log(tableName.DBT_PHZ_DESK_ROUND_ALL).Page(bson.M{
+		"userids": bson.RegEx{querKey, "."},
+	}, &deskRecords, "-deskid", 1, 20)
+
+	if deskRecords == nil || len(deskRecords) <= 0 {
+		log.T("没有找到玩家[%v]跑胡子相关的战绩...", userId)
+		return nil
+	} else {
+		return deskRecords
+	}
+}
+
+//查询牌桌内战绩
+func GetPhzDeskRoundByDeskId(userId uint32, deskId int32) []T_phz_desk_round {
+	var deskRecords []T_phz_desk_round
+	querKey, _ := numUtils.Uint2String(userId)
+	db.Log(tableName.DBT_PHZ_DESK_ROUND).Page(bson.M{
+		"userids": bson.RegEx{querKey, "."},
+		"deskid":  deskId,
+	}, &deskRecords, "-gamenumber", 1, 20)
+
+	if deskRecords == nil || len(deskRecords) <= 0 {
+		log.T("没有找到玩家[%v]跑胡子相关的牌桌内战绩...", userId)
+		return nil
+	} else {
+		return deskRecords
+	}
+}
--- common/phzData/Desk.go
+++ common/phzData/Desk.go
@@ -5,7 +5,6 @@ import (
 	"casino_common/utils/db"
 	"github.com/name5566/leaf/module"
 	"sync"
-	"time"
 )
 
 //desk
@@ -22,22 +21,23 @@ type Desk struct {
 
 //deskCfg
 type DeskCfg struct {
-	Owner            uint32    //房主
-	Banker           uint32    //庄家
-	NextBanker       uint32    //下一个庄家
-	CreateFee        int64     //房费
-	GameNumber       int32     //游戏编号
-	ActiveUser       uint32    //活动玩家
-	BoardsCout       int32     //总场次数
-	UserCountLimit   int32     //玩家的人数
-	CurrRound        int32     //当前的场次数
-	Password         string    //房间号
-	BeginTime        time.Time //开始时间
-	RoomType         int32     //房间类型
-	TotalPokerNum    int32     //扑克牌
-	HandPokerInitNum int32     //初始手牌的数量
-	ApplyDis         bool      //申请解散状态
-	HuXi             int32     //胡息数
+	Owner            uint32 //房主
+	Banker           uint32 //庄家
+	NextBanker       uint32 //下一个庄家
+	CreateFee        int64  //房费
+	GameNumber       int32  //游戏编号
+	ActiveUser       uint32 //活动玩家
+	BoardsCout       int32  //总场次数
+	UserCountLimit   int32  //玩家的人数
+	CurrRound        int32  //当前的场次数
+	Password         string //房间号
+	BeginTime        string //开始时间
+	EndTime          string //结束时间
+	RoomType         int32  //房间类型
+	TotalPokerNum    int32  //扑克牌
+	HandPokerInitNum int32  //初始手牌的数量
+	ApplyDis         bool   //申请解散状态
+	HuXi             int32  //胡息数
 	PaiCursor        int32
 	CardsNum         int32  //手牌数量
 	IsDaiKai         bool   //是否代开
