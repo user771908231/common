7eaa64d94d169d20f0de969e8d49173451f1d2e8
james wang <yiming.520@live.com>
2017-11-11 17:35:44 +0800

    昨日佣金计算

--- common/model/agentModel/rebate_coin_log.go
+++ common/model/agentModel/rebate_coin_log.go
@@ -70,3 +70,27 @@ func GetAgentCoinFeeRebateToday(agent_id uint32) float64 {
 
 	return numUtils.Float64Format(today_rebate.Sum)
 }
+
+//获取昨日佣金
+func GetAgentCoinFeeRebateYesterday(agent_id uint32) float64 {
+	var today_rebate struct{
+		Sum float64
+	}
+	//今日佣金
+	today,_ := time.Parse("2006-01-02", time.Now().AddDate(0,0,-1).Format("2006-01-02"))
+	db.Log(tableName.DBT_AGENT_COIN_FEE_REBATE_LOG).Pipe([]bson.M{
+		bson.M{"$match":bson.M{
+			"agentid": bson.M{"$eq": agent_id},
+			"time": bson.M{
+				"$gte": today,
+				"$lt": today.AddDate(0,0,1),
+			},
+		}},
+		bson.M{"$group":bson.M{
+			"_id": nil,
+			"sum": bson.M{"$sum": "$rebate"},
+		}},
+	}, &today_rebate)
+
+	return numUtils.Float64Format(today_rebate.Sum)
+}
